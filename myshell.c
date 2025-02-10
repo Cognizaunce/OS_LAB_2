26675fc019e84c540127fc87648641b7c7328743
RivkaRSagi <rivka.r.sagi@gmail.com>
2025-02-10 16:56:05 -0500

    added if-else ladder for command recognition

--- myshell.c
+++ myshell.c
@@ -5,11 +5,29 @@ void execute_command(char *input) {
     char *delim = " \n";
     token = strtok(input, delim);
     if (token != NULL) {
-        if (strcmp(token, "cd") == 0) {
-            token = strtok(NULL, delim); // Get next part
-            (directory path)
+        if (strncasecmp(token, "cd") == 0) { //cd
+            token = strtok(NULL, delim); // Get next part (directory path)
             change_directory(token);
-        } else {
+        } else if (strncasecmp(token, "clr")== 0){ //clr
+            clear();
+        } else if(strncasecmp(token, "dir") == 0){ // dir
+            token = strtok(NULL, delim);
+            directory(token);
+        } else if(strncasecmp(token, "environ") == 0){ //environ
+            environment_variables();
+        } else if(strncasecmp(token, "echo") == 0){ //echo
+            token = strtok(NULL, delim);
+            echo(token);
+        } else if(strncasecmp(token, "help") ==0){ // help
+            token = strtok(NULL, delim);
+            help(token);
+        } else if (strncasecmp(token, "pause") == 0){ // pause
+            token = strtok(NULL, delim);
+            pause(token);
+        }else if (strncasecmp(token, "quit") == 0){ // quit
+            quit();
+        }
+        else {
         printf("Command not recognized.\n");
         }
     }
