17470fca2a2e3f445f3f0f58f3608c74ba893547
RivkaRSagi <rivka.r.sagi@gmail.com>
2025-02-12 15:49:56 -0500

    added function definitions to header file

--- myshell.h
+++ myshell.h
@@ -5,7 +5,10 @@
 #include <string.h> // String handling functions
 #include <unistd.h> // Provides access to the POSIX operating system API
 
-void change_directory(char *path); // declaration of 'cd' function
-(cd function prototype)
+void change_directory(char *path); // declaration of 'cd' function (cd function prototype)
+
+void clear();
+
+void directory(char *path);
 
 #endif
