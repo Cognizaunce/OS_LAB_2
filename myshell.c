68caa67568b0fa48112cb8c3bf33ae06c66a658d
Iqra Zahid <110575291+Iqra-Z@users.noreply.github.com>
2025-02-13 11:36:38 -0500

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -1,51 +1,83 @@
 #include "myshell.h"
 
-void execute_command(char *input) {
+void execute_external_command(char *input) {
     pid_t pid;
     int status;
-    char *args[100]; // array to hold command arguments
+    char *args[100]; // Array to hold command arguments
     char *token;
     char *delim = " \n";
     int i = 0;
     
     token = strtok(input, delim);
+    while (token != NULL && i < 99) {
+        args[i++] = token;
+        token = strtok(NULL, delim);
+    }
+    args[i] = NULL;
+    
+    if (args[0] == NULL) {
+        return; // No command entered
+    }
+    
+    pid = fork();
+    if (pid == -1) {
+        perror("fork failed");
+        return;
+    }
+    
+    if (pid == 0) { // Child process
+        setenv("parent", getenv("SHELL"), 1); // Set parent environment variable
+        if (execvp(args[0], args) == -1) {
+            perror("Execution failed");
+            exit(EXIT_FAILURE);
+        }
+    } else { // Parent process
+        waitpid(pid, &status, 0);
+    }
+}
+
+void execute_command(char *input) {
+    char *token;
+    char *delim = " \n";
+    token = strtok(input, delim);
     if (token != NULL) {
-        if (strncasecmp(token, "cd") == 0) { //cd
-            token = strtok(NULL, delim); // Get next part (directory path)
+        if (strcasecmp(token, "cd") == 0) {
+            token = strtok(NULL, delim);
             change_directory(token);
-        } else if (strncasecmp(token, "clr")== 0){ //clr
+        } else if (strcasecmp(token, "clr") == 0) {
             clear();
-        } else if(strncasecmp(token, "dir") == 0){ // dir
+        } else if (strcasecmp(token, "dir") == 0) {
             token = strtok(NULL, delim);
             directory(token);
-        } else if(strncasecmp(token, "environ") == 0){ //environ
+        } else if (strcasecmp(token, "environ") == 0) {
             environment_variables();
-        } else if(strncasecmp(token, "echo") == 0){ //echo
+        } else if (strcasecmp(token, "echo") == 0) {
             token = strtok(NULL, delim);
             echo(token);
-        } else if(strncasecmp(token, "help") ==0){ // help
+        } else if (strcasecmp(token, "help") == 0) {
             token = strtok(NULL, delim);
             help(token);
-        } else if (strncasecmp(token, "pause") == 0){ // pause
+        } else if (strcasecmp(token, "pause") == 0) {
             token = strtok(NULL, delim);
             pause(token);
-        }else if (strncasecmp(token, "quit") == 0){ // quit
+        } else if (strcasecmp(token, "quit") == 0) {
             quit();
-        }
-        else {
-        printf("Command not recognized.\n");
+        } else {
+            execute_external_command(input); // Handle external commands
         }
     }
 }
 
 int main() {
-char input[1024]; // Buffer for user input
-while (1) {
-    printf("MyShell> ");
-    if (fgets(input, sizeof(input), stdin) == NULL) {
-        break; // Exit on EOF
+    char input[1024];
+    setenv("SHELL", "/bin/myshell", 1); // Set shell environment variable
+    
+    while (1) {
+        printf("MyShell> ");
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break; // Exit on EOF
+        }
+        execute_command(input);
     }
-    execute_command(input); // Process the input
-}
-return 0;
+    return 0;
 }
