b5fa905c1178ea61d135860151f36a8c6f58c67d
RivkaRSagi <rivka.r.sagi@gmail.com>
2025-02-13 19:53:50 -0500

    remove unnecessary files

--- string_acquire.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRING_ACQUIRE_H
-#define STRING_ACQUIRE_H
-// Below, only include the headers necessary to interface with the `string_acquire` function
-#include <stdio.h>  // For `FILE*`, part of the C standard library
-
-// Function prototype
-char* string_acquire(char* s, int size, FILE* stream);
-
-#endif // STRING_ACQUIRE_H
